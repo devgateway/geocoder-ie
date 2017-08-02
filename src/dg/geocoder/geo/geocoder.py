0c20a57a89ed0c2d7faeb1b724ea6b5e8e5c678c
Sebastian Dimunzio <sdimunzio@gmail.com>
2017-08-02 16:58:05 -0300

    improving composite locations merge, and adding some initial unit test

--- src/dg/geocoder/geo/geocoder.py
+++ src/dg/geocoder/geo/geocoder.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pycorenlp.corenlp
-from sner import Ner
 
 from dg.geocoder.classification.classifier import load_classifier
 from dg.geocoder.config import get_ner_host, get_ner_port
-from dg.geocoder.geo.result_parser import CodingResults
-from dg.geocoder.readers.factory import get_reader, get_text_reader
+from dg.geocoder.readers.factory import get_text_reader, get_reader
 
 
 def process_activity(activity):
@@ -19,81 +17,98 @@ def bulk_process(xml, path_to_docs='docs'):
         print('Process list of activities')
 
 
-def classify(sentences, cls_name='default_classifi2er'):
+def classify(text=None, file=None, cls_name='default_classifier'):
+    reader = None
+    if file is not None:
+        reader = get_reader(file)
+    else:
+        reader = get_text_reader(text)
+
+    # split document in sentences
+    sentences = reader.split()
     classifier = load_classifier(cls_name)
     predicted = classifier.predict(sentences)
-    return np.where(predicted == 'geography')[0]
-
-
-def classify_document(file, **kwargs):
-    reader = get_reader(file)
-    # split document in stences
-    sentences = reader.split()
-    indexes = classify(sentences, **kwargs)
+    indexes = np.where(predicted == 'geography')[0]
     geo_sentences = [(sentences[i]) for i in indexes]
     return geo_sentences
 
 
-def classify_text(text, **kwargs):
-    reader = get_text_reader(text)
-    # split document in stences
-    sentences = reader.split()
-    indexes = classify(sentences, **kwargs)
-    geo_sentences = [(sentences[i]) for i in indexes]
-    return geo_sentences
+# query geonames an get geographical information
+def geonames(list, country_code=None):
+    for row in list:
+        locations = row['locations']
+        relations = row['relations']
+        for l in locations:
+            print(l)
 
 
-def get_relations(output, location):
-    result = [output["sentences"][0]["openie"] for item in output]
-    rels = []
-    for i in result:
-        for rel in i:
-            if rel['subject'] == location or rel['object'] == location:
-                rels.append((rel['subject'], rel['relation'], rel['object']))
-
-    return rels
-
-
-def nlp(sentences):
+# Perform natural language processing to text, get annotated entities and entities relations
+def extract(sentences):
     nlp = pycorenlp.corenlp.StanfordCoreNLP("http://{0}:{1}/".format(get_ner_host(), get_ner_port()))
-    tagged_sentences = []
+    extraction = []
 
     for s in sentences:
         output = nlp.annotate(s.replace('\n', ' '), properties={"annotators": "openie,ner", "outputFormat": "json"})
-        locations_found = [(t['originalText'], get_relations(output, t['originalText'])) for t in
-                           output["sentences"][0]["tokens"]
-                           for item in output if
+        relations = [output["sentences"][0]["openie"] for item in output]
+        locations_found = [(t['originalText']) for t in output["sentences"][0]["tokens"] for item in output if
                            t['ner'] == 'LOCATION']
 
         if len(locations_found) > 0:
-            tagged_sentences.append((s, locations_found))
+            extraction.append(({'text': s, 'entities': locations_found, 'relations': relations}))
 
-    return tagged_sentences
+    return extraction
 
 
-def tag_sentences(sentences):
-    tagger = Ner(host=get_ner_host(), port=get_ner_port())
-    tagged_sentences = []
-    for s in sentences:
-        tagged = tagger.get_entities(s.replace('\n', ' '))
-        locs = [(word.strip()) for word, tag in tagged if tag in ['LOCATION']]
-        if len(locs) > 0:
-            tagged_sentences.append((s, locs))
-    return tagged_sentences
+def gap_length(word1, word2, text):
+    start_1 = text.index(word1)
+    start_2 = text.index(word2)
+
+    end_1 = start_1 + len(word1)
+    end_2 = start_2 + len(word2)
+    gap = start_2 - end_1
+    connection_text = text[end_1:start_2]
+    edited_text = chr(0) * end_1 + text[end_1:]
+    return gap, connection_text, edited_text
+
+
+def merge(extracted, distance=2, ignored_gap_chars=[',', '-']):
+    ret_val = []
+    for row in extracted:
+        text = row['text']
+        entities = row['entities']
+        x = 0
+        last_idx = len(entities) - 1
+        merged = []
+
+        # ierate locations
+        while x <= last_idx:
+            to_merge = entities[x]
+
+            while x is not last_idx:
+                current_loc = entities[x]
+                next_loc = entities[x + 1]
+                # get the gap bewteen the two entities
+                gap, connection_text, text = gap_length(current_loc, next_loc, text)
+                # if gap is < to distance but there are special chars in between we take them as separate
+                # entities ie Congo,Burkina will be taken as ['Congo','Burkina'] and Congo,New Guinea,
+                # Burkina will be taken as ['Congo','New Guinea','Burkina']
+                if gap < distance and connection_text not in ignored_gap_chars:
+                    to_merge += ' ' + next_loc
+                    x += 1
+                else:
+                    break
+            x += 1
+            merged.append(to_merge)
+        row['locations'] = merged
+    return extracted
 
 
 def geocode(text=None, file=None, cls_name='default_classifier'):
     texts = ''
     results = None
+    # 1) classify paragraph and filter out what doesn't refer to project geographical information
+    # 2) extract entities and relationships
+    # 3) merge names
+    # 3) resolve locations using Geo Names
 
-    if text is not None:
-        texts = classify_text(text, cls_name=cls_name)
-        results = CodingResults(nlp(texts)).get_results()
-
-    elif file is not None:
-        texts = classify_document(file, cls_name=cls_name)
-        results = CodingResults(nlp(texts)).get_results()
-    else:
-        raise ValueError('you must provide a file or a text input ')
-
-    return results
+    return geonames(merge(extract(classify(text=text, file=file, cls_name=cls_name))))
--- test/test_geocoder.py
+++ test/test_geocoder.py
@@ -1,16 +1,27 @@
 import unittest
 
-from dg.geocoder.geo.geocoder import geocode
+from dg.geocoder.geo.geocoder import geocode, merge
 
 
 class TestGeocoder(unittest.TestCase):
     def test_geocode(self):
         text = """During this period, PRIME expanded to include 4 new upazilas 
                     (Tala in Satkhira district and Islampur, Dewanganj and Melandaha in Jamalpur district) 
-                        to serve large numbers of poor people in these adjacent areas."""
+                        to serve large numbers of poor people in these adjacent areas"""
+        geo = geocode(text=text)
 
-        print(geocode(text=text))
+    def test_merge(self):
+        text = "Congo,New Guinea and Burkina Faso, same as the route Cordoba-Catamarca, Democratic Republic of Congo"
+        entities = ['Congo', 'New', 'Guinea', 'Burkina Faso', 'Cordoba', 'Catamarca', 'Democratic', 'Republic', 'of',
+                    'Congo']
+        rels = []
+        extraction = []
+        extraction.append(({'text': text, 'entities': entities, 'relations': []}))
+        results = merge(extraction)
+        self.assertTrue('New Guinea' in results[0]['locations'])
+        self.assertTrue('Cordoba' in results[0]['locations'])
+        self.assertTrue('Burkina Faso' in results[0]['locations'])
+        self.assertTrue('Democratic Republic of Congo' in results[0]['locations'])
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
