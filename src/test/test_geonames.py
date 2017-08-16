c69ced44ad12afbe1bdfa1e530cf69e125b7499b
Sebastian Dimunzio <sdimunzio@gmail.com>
2017-08-15 23:50:09 -0300

    initial geonames unit testing

---
+++ src/test/test_geonames.py
@@ -0,0 +1,17 @@
+import unittest
+
+from dg.geocoder.geo.geonames import resolve
+
+
+class TestGeoNames(unittest.TestCase):
+    def test_country_name(self):
+        resolve('Kenya', cty_codes=[])
+
+    def test_incomplete_name(self):
+        resolve('Dhaka South', cty_codes=[])['name']=='Dhaka South City Corporation'
+
+    def test_strange_name(self):
+        resolve('FSD', cty_codes=[])
+
+if __name__ == '__main__':
+    unittest.main()
