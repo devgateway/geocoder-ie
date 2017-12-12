824602b3f14bad8208c27942e2a9d55b10fd7d38
Ionut Dobre <idobre@developmentgateway.org>
2017-12-12 13:07:31 +0200

    create bash scripts + change logging folder.

---
+++ ner.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -server -Xms256m -Xmx1024m -XX:MaxMetaspaceSize=256m -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=128m -Djava.awt.headless=true -XX:+CMSParallelRemarkEnabled -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -Xverify:none -noverify -cp ner/stanford-ner-2017-06-09/stanford-ner-3.8.0.jar edu.stanford.nlp.ie.NERServer -loadClassifier ner/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz -port 9094
